d4ce5ab7dbb4ded068b132569692c481607d2975
tugalsan karabacak <tugalsan@gmail.com>
2022-09-21 16:11:47 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.tugalsan.trm.trainer {
+    requires gwt.user;
+    requires java.desktop;
+    requires AbsoluteLayout.RELEASE120;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.string;
+    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.cast;
+    requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.shape;
+    requires com.tugalsan.api.input;
+    requires com.tugalsan.api.file;
+    requires com.tugalsan.api.file.txt;
+}
