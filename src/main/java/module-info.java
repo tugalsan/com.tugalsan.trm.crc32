be33176e863aad05beb073fa8f79000999c09128
tugalsan karabacak <tugalsan@gmail.com>
2022-09-21 16:44:06 +0300

    first release

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,13 @@
-module com.tugalsan.trm.trainer {
+module com.tugalsan.trm.crc32 {
     requires gwt.user;
-    requires java.desktop;
-    requires AbsoluteLayout.RELEASE120;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.time;
     requires com.tugalsan.api.cast;
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.shape;
-    requires com.tugalsan.api.input;
     requires com.tugalsan.api.file;
-    requires com.tugalsan.api.file.txt;
+    requires com.tugalsan.api.file.xlsx;
+    requires com.tugalsan.api.desktop;
 }
