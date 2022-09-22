9ec919189d196a2c73035e22c6d3f95e73344668
tugalsan karabacak <tugalsan@gmail.com>
2022-09-22 16:42:56 +0300

    bug fix

---
+++ src/main/java/com/tugalsan/trm/crc32/Main.java
@@ -0,0 +1,40 @@
+package com.tugalsan.trm.crc32;
+
+import com.tugalsan.api.desktop.server.TS_DesktopUtils;
+import com.tugalsan.api.file.csv.server.TS_FileCsvUtils;
+import com.tugalsan.api.file.server.TS_DirectoryUtils;
+import com.tugalsan.api.file.server.TS_FileUtils;
+import com.tugalsan.api.list.client.TGS_ListTable;
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String... args) {
+        var folder = TS_DesktopUtils.chooseFolder(Path.of("C:\\Program Files\\SOLIDWORKS Corp"));
+        if (folder == null) {
+            TS_DesktopUtils.info("folder == null -> exit;");
+            System.exit(1);
+            return;
+        }
+        var output = Path.of("crc32.xlsx");
+
+        var table = TGS_ListTable.of(true);
+        TS_DirectoryUtils
+                .subFiles(folder, "*.*", true, true)
+                .forEach(file -> {
+                    var create = TS_FileUtils.getTimeCreationTime(file);
+                    var modified = TS_FileUtils.getTimeLastModified(file);
+                    table.setValue(
+                            table.getRowSize(),
+                            String.valueOf(TS_FileUtils.getChecksumHex(file)),
+                            create == null ? "null" : create.toString_dateOnly(),
+                            modified == null ? "null" : modified.toString_dateOnly(),
+                            file.toString()
+                    );
+                });
+        TS_FileCsvUtils.toFile(table, output, true);
+
+        TS_DesktopUtils.info("See " + output.toString());
+        System.exit(0);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,14 @@ module com.tugalsan.trm.crc32 {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.list;
     requires com.tugalsan.api.time;
     requires com.tugalsan.api.cast;
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.shape;
     requires com.tugalsan.api.file;
-    requires com.tugalsan.api.file.xlsx;
+    requires com.tugalsan.api.file.csv;
     requires com.tugalsan.api.desktop;
+    requires java.desktop;
+    requires java.logging;
 }
