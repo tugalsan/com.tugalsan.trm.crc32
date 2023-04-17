c3d655d77527363fe4f0e33f1aef2d0f43d97c9c
tugalsan karabacak <tugalsan@gmail.com>
2023-04-17 13:52:56 +0300

    .

--- src/main/java/com/tugalsan/trm/crc32/Main.java
+++ src/main/java/com/tugalsan/trm/crc32/Main.java
@@ -1,26 +1,29 @@
 package com.tugalsan.trm.crc32;
 
+import com.tugalsan.api.desktop.server.TS_DesktopDialogInfoUtils;
 import com.tugalsan.api.desktop.server.TS_DesktopPathUtils;
 import com.tugalsan.api.file.csv.server.TS_FileCsvUtils;
 import com.tugalsan.api.file.server.TS_DirectoryUtils;
 import com.tugalsan.api.file.server.TS_FileUtils;
 import com.tugalsan.api.list.client.TGS_ListTable;
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class Main {
 
     public static void main(String... args) {
-        var folder = TS_DesktopPathUtils.chooseFolder(Path.of("C:\\Program Files\\SOLIDWORKS Corp"));
-        if (folder == null) {
-            TS_DesktopPathUtils.info("folder == null -> exit;");
+        var folder = TS_DesktopPathUtils.chooseDirectory("", Optional.of(Path.of("C:\\Program Files\\SOLIDWORKS Corp")));
+        if (folder.isEmpty()) {
+            TS_DesktopDialogInfoUtils.show("", "folder == null -> exit;");
             System.exit(1);
             return;
         }
+
         var output = Path.of("crc32.xlsx");
 
         var table = TGS_ListTable.of(true);
         TS_DirectoryUtils
-                .subFiles(folder, "*.*", true, true)
+                .subFiles(folder.get(), "*.*", true, true)
                 .forEach(file -> {
                     var create = TS_FileUtils.getTimeCreationTime(file);
                     var modified = TS_FileUtils.getTimeLastModified(file);
@@ -34,7 +37,7 @@ public class Main {
                 });
         TS_FileCsvUtils.toFile(table, output, true);
 
-        TS_DesktopPathUtils.info("See " + output.toString());
+        TS_DesktopDialogInfoUtils.show("", "See " + output.toString());
         System.exit(0);
     }
 }
